27c7df596b60df6d78600a0b52eaeb3c30df39fa
Bhakta Jason <contact@newmayapur.com>
2024-10-28 19:57:18 +0100

    optimising the ffmpeg settings

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -4,6 +4,7 @@ RTSP_URL="rtsp://admin:Gaura108@192.168.86.215:554/11"
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
 YOUTUBE_KEY="74vx-xdv7-fxth-7wrr-7dcf"
 
-#screen -d -m 
-#TODO: optimise this ffmpeg command
-ffmpeg -rtsp_transport tcp -i ${RTSP_URL} -tune zerolatency -vcodec libx264 -pix_fmt + -c:v copy -c:a aac -strict experimental -f flv ${YOUTUBE_URL}/${YOUTUBE_KEY}
+ffmpeg -rtsp_transport tcp -i ${RTSP_URL} \
+  -tune zerolatency -preset ultrafast -f flv \
+  -vcodec libx264 -pix_fmt yuv420p -b:v 2M -maxrate 2M -bufsize 4M \
+  -c:a aac -b:a 128k -f flv ${YOUTUBE_URL}/${YOUTUBE_KEY}
